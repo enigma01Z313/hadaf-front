a96ac55b57679e37de941217fc6db9266c1d1bcf
Farzin <farzin@Farzins-MacBook-Air.local>
2023-12-16 12:43:28 +0330

    ssss

--- src/app/lib/request.js
+++ src/app/lib/request.js
@@ -2,7 +2,7 @@ import { toast } from "react-toastify";
 
 const request = (payload) =>
   new Promise(async (resolve, reject) => {
-    const BASE_HOST = "http://67.43.234.92:50000";
+    const BASE_HOST = "https://hadafsanjapi.farzinahmadi.com";
     const BASE_URL = `${BASE_HOST}/api`;
     const USER_TOKEN = "f8b874cf9ad70805465c6d93ab1231b";
 
