d87ca9763d5e3f19c51305dcef7a6bea4c50eb30
Farzin <farzin@Farzins-MacBook-Air.local>
2024-01-09 00:40:48 +0330

    task single

--- src/app/dashboard/tasks/components/Single/Left/Colleages.jsx
+++ src/app/dashboard/tasks/components/Single/Left/Colleages.jsx
@@ -2,6 +2,6 @@ import React from "react";
 
 export default function Colleages({colleages}) {
   return (
-    <div className="wrapper-box2 cursor-pointer">همکاران: </div>
+    <div className="wrapper-box2 cursor-pointer">همکاران: </div> 
   );
 }
