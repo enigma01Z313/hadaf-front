f630c40a545d857f2ea067c41a0dd1e89443fb7d
Farzin <farzin@Farzins-MacBook-Air.local>
2024-01-23 22:04:29 +0330

    next icon

--- src/app/dashboard/workspaces/[id]/Timeframe/TimeFrameItem.jsx
+++ src/app/dashboard/workspaces/[id]/Timeframe/TimeFrameItem.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import KeyboardBackspaceIcon from "@mui/icons-material/KeyboardBackspace";
 
 import Devider from "@/app/components/Devider";
 import styles from "./style.module.css";
@@ -7,14 +8,15 @@ export default function TimeFrameItem({ timeframe, setSingle }) {
   return (
     <article
       className={`wrapper-box grow-1 mb-2 cursor-pointer
-        ${styles['timeframe-item']}`}
+        ${styles["timeframe-item"]}`}
       onClick={() => setSingle(timeframe.id)}>
       <p className="text-h6 weight-500">{timeframe.title}</p>
       <Devider line={true} spacing={1} />
       <p>{timeframe.description}</p>
       <Devider line={true} spacing={1} />
-      <div>
-        {new Date(timeframe.startDate).toLocaleDateString("fa-IR")} {"->"}{" "}
+      <div className="d-flex align-center">
+        {new Date(timeframe.startDate).toLocaleDateString("fa-IR")}{" "}
+        <KeyboardBackspaceIcon className="mx-0-5" style={{width: "20px"}} />
         {new Date(timeframe.endDate).toLocaleDateString("fa-IR")}
       </div>
     </article>
