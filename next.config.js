4bde3c0a5a03ed9768e22b9b344e09df69ad2612
enigma01Z313 <f.ahmadyf94@gmail.com>
2024-02-26 09:29:34 +0330

    online setup

--- next.config.js
+++ next.config.js
@@ -1,14 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // output: "export",
-  // experimental: {
-  //   urlImports: [
-  //     "http://67.43.234.92:50000",
-  //     "http://67.43.234.92:50001",
-  //     "https://hadafsanjapi.farzinahmadi.com",
-  //     "https://hadafsanj.farzinahmadi.com",
-  //   ],
-  // },
+  //output: "export",
+  experimental: {
+    urlImports: [
+      "http://67.43.234.92:50000",
+      "http://67.43.234.92:40000",
+      "http://67.43.234.92:50001",
+      "https://hadafsanjapi.farzinahmadi.com",
+      "https://hadafsanj.farzinahmadi.com",
+    ],
+  },
 };
 
 module.exports = nextConfig;
