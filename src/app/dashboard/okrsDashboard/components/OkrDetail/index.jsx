8527a7c521b2bfb8fbec8455c805fd7f37b676f2
Farzin <farzin@Farzins-MacBook-Air.local>
2024-02-27 11:27:31 +0330

    s

--- src/app/dashboard/okrsDashboard/components/OkrDetail/index.jsx
+++ src/app/dashboard/okrsDashboard/components/OkrDetail/index.jsx
@@ -1,5 +1,10 @@
+"use client"
+
 import React from "react";
-import "boxicons";
+
+// import "boxicons";
+
+import OkrDetailItem from "./OkrDetailItem";
 
 const OkrDetailItemConfigs = [
   {
@@ -27,29 +32,6 @@ const OkrDetailItemConfigs = [
     color: { bg: "#d2ffe8", text: "#39da8a" },
   },
 ];
-
-function OkrDetailItem({ title, value, icon, color, index }) {
-  return (
-    <div
-      className={`wrapper-box grow-1 d-flex direction-column align-center 
-        ${index !== 0 ? "mr-3" : ""}`}>
-      <div>
-        <box-icon color={color.text} name={icon}></box-icon>
-      </div>
-      <div className="mb-2 text-body-2">{title}</div>
-      <div
-        className="p-1 text-center"
-        style={{
-          background: color.bg,
-          color: color.text,
-          minWidth: "43px",
-          borderRadius: "43px",
-        }}>
-        {value}
-      </div>
-    </div>
-  );
-}
 
 export default function OkrDetail({ data }) {
   return (
